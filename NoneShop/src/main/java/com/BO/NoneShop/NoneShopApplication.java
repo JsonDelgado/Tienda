d1244301fa127ae9b58a70c3690fad270831c8bf
Json <90729423+JsonDelgado@users.noreply.github.com>
2021-09-18 08:40:26 -0500

    Subida de proyecto
    Subida de proyecto

---
+++ NoneShop/src/main/java/com/BO/NoneShop/NoneShopApplication.java
@@ -0,0 +1,13 @@
+package com.BO.NoneShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoneShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoneShopApplication.class, args);
+	}
+
+}
