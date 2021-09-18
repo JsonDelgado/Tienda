a82636afa1ed5119798963658c32921b3b2d242f
EduynCubillos <90782533+EduynCubillos@users.noreply.github.com>
2021-09-18 09:02:12 -0500

    Creación clase Conexión
    Se crea clase Conexión

---
+++ NoneShop/src/main/java/com/DAO/noneShop/Conexion.java
@@ -0,0 +1,45 @@
+package com.DAO.noneShop;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+
+public class Conexion {
+
+	static String bd = "tiendap";
+	static String login = "root";
+	static String password="1234";
+	static String url = "jdbc:mysql://localhost/"+bd;
+
+	Connection connection = null;
+	
+	public Conexion() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");			
+			connection = DriverManager.getConnection(url,login,password);
+			
+			if(connection!=null) {
+				System.out.println("Conexión ok");
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		} catch(ClassNotFoundException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public Connection getConnection() {
+		return connection;
+		
+	}
+	public void desconectar() {
+		connection =null;
+	}
+	
+	
+}
